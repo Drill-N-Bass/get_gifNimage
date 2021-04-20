31a81ae70541caf491b002d39d24547149254862
Paul Pedryc <77461241+Drill-N-Bass@users.noreply.github.com>
2021-04-20 22:44:04 +0200

    Add files via upload

---
+++ get_gif_n_image/Container/get_gif_n_image/setup.py
@@ -0,0 +1,19 @@
+﻿import setuptools
+with open("README.md", "r") as fh:
+    longdescription « fh.read()
+setuptools.setup(
+	name=“get_gif_n_image", 
+	version=“0.0.1”, 
+	author=”Paul Pedryc", 
+	author_email='pawelpedryc@gmail.com', 
+	description="A small graphics library", 
+	long_description=long_description, 
+	long_description_content_type=“text/raarkdown 
+	url="https://github.com/Drill-N-Bass/get_gif_n_image.git",
+	packages=setuptools.find packages(),             ##############Tu skończyłeś
+	classifiers=[
+		"Progranming Language :: Python :: 3”,
+		“License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires = '>=3.6'
