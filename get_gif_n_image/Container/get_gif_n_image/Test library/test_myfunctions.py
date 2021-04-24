5d9974ed0bab463c78bdd618332f967c3e2bbcbf
Paul Pedryc <77461241+Drill-N-Bass@users.noreply.github.com>
2021-04-25 00:09:50 +0200

    Delete get_gif_n_image/Container/get_gif_n_image directory

--- get_gif_n_image/Container/get_gif_n_image/Test library/test_myfunctions.py
+++
@@ -1,3 +0,0 @@
-from mypythonlib import myfunctions
-def get_gif_n_image():
-    assert myfunctions.get_gif_n_image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Devon_rex_juvenile_Cat.jpg/576px-Devon_rex_juvenile_Cat.jpg")
